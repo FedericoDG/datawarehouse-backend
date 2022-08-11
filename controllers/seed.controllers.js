cfef910400eff4fe937f4dff5fad14311f7762b1
Federico Gonzalez <alambratore@hotmail.com>
2022-08-11 08:52:00 -0300

    Added a try/catch block

--- controllers/seed.controllers.js
+++ controllers/seed.controllers.js
@@ -3,13 +3,19 @@ const fs = require('fs');
 const sqlFileExecuter = require('../helpers/sqlFileExecutor');
 
 const seed = async (_, res) => {
-  const SQL_DUMP_FILE = fs.readFileSync('./database/datawarehouse.sql', 'utf-8');
-
-  sqlFileExecuter(SQL_DUMP_FILE);
-
-  res.json({
-    message: 'La base de datos ha sido regenerada con éxito'
-  });
+  try {
+    const SQL_DUMP_FILE = fs.readFileSync('./database/datawarehouse.sql', 'utf-8');
+    sqlFileExecuter(SQL_DUMP_FILE);
+    res.json({
+      message: 'La base de datos ha sido regenerada con éxito'
+    });
+  } catch (error) {
+    res.status(500).json({
+      response: false,
+      message: 'Ha ocurrido un error.',
+      error
+    });
+  }
 };
 
 module.exports = {
